0e7687063d3f6fea1e321d67b0018e980e11b791
Nisarg <nisargs7798@gmail.com>
2020-11-30 12:03:14 -0500

    OTP Activity

---
+++ app/src/main/java/com/example/doorstep/loginSignup/VerifyOtpActivity.java
@@ -0,0 +1,16 @@
+package com.example.doorstep.loginSignup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.doorstep.R;
+
+public class VerifyOtpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_verify_otp);
+    }
+}
