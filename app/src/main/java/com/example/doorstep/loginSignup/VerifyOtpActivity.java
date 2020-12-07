8e70b7429448f10db6b2170c6c255d26a7775e3c
Nisarg <nisargs7798@gmail.com>
2020-12-07 13:28:36 -0500

    OTP Activity

--- app/src/main/java/com/example/doorstep/loginSignup/VerifyOtpActivity.java
+++ app/src/main/java/com/example/doorstep/loginSignup/VerifyOtpActivity.java
@@ -2,15 +2,31 @@ package com.example.doorstep.loginSignup;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
+import com.example.doorstep.Home.HomeActivity;
 import com.example.doorstep.R;
 
 public class VerifyOtpActivity extends AppCompatActivity {
+
+    Button btnVerify;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_verify_otp);
+
+        btnVerify = findViewById(R.id.btn_verify);
+
+        btnVerify.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(VerifyOtpActivity.this, HomeActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
